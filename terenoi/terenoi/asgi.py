416398674921e3c44b183ba585e473fbbebee220
olegmaslov <oleg.gnome@yandex.ru>
2022-02-01 15:21:16 +0300

    asgi settings

--- terenoi/terenoi/asgi.py
+++ terenoi/terenoi/asgi.py
@@ -8,13 +8,19 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 """
 
 import os
-from channels.routing import ProtocolTypeRouter, URLRouter
+
+import django
 from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'terenoi.settings')
+django.setup()
+from channels.routing import ProtocolTypeRouter, URLRouter
+
 from django.urls import path
 from django_channels_jwt_auth_middleware.auth import JWTAuthMiddlewareStack
 from notifications import consumers
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'terenoi.settings')
+
 
 ws_patterns = [
     path('ws/notifications/', consumers.NotificationConsumer.as_asgi())
