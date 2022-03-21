7ba6345de78a7f64111d5d9f6d4946fcfd6d6184
julia <sorulaijuli@gmail.ru>
2022-03-21 16:51:15 +0300

    create TeacherBillData model, create teacher finance view and serializers, create teacher historypayment serializer and view[#644]

--- terenoi/finance/serializers.py
+++ terenoi/finance/serializers.py
@@ -1,10 +1,12 @@
-from django.db.models import Sum, Q
+from django.db.models import Q
 from rest_framework import serializers
 
-from finance.models import StudentBalance, HistoryPaymentStudent
+from finance.models import StudentBalance, HistoryPaymentStudent, TeacherBankData, TeacherBalance, \
+    HistoryPaymentTeacher, TeacherRate
 from lessons.models import Lesson
 from lessons.services import current_date
-from profileapp.models import Subject
+from profileapp.models import TeacherSubject
+from settings.models import RateTeachers
 
 
 class StudentBalanceSerializer(serializers.ModelSerializer):
@@ -67,5 +69,109 @@ class HistoryPaymentStudentSerializer(serializers.ModelSerializer):
     def get_subject_name(self, instance):
         if instance.subject:
             return instance.subject.name
+        else:
+            return None
+
+
+class BillSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TeacherBankData
+        fields = ('pk', 'bank_name', 'bik', 'bill', 'full_teacher_name', 'card')
+
+
+class TeacherBalanceSerializer(serializers.ModelSerializer):
+    date = serializers.SerializerMethodField()
+    rate = serializers.SerializerMethodField()
+    bill = serializers.SerializerMethodField()
+
+    class Meta:
+        model = TeacherBalance
+        fields = ('money_balance', 'currency', 'date', 'rate', 'bill')
+
+    def _user(self):
+        request = self.context.get('request', None)
+        if request:
+            return request.user
+        return None
+
+    def get_date(self, instance):
+        date = HistoryPaymentTeacher.objects.filter(teacher=self._user(), ).order_by(
+            '-payment_date').first().payment_date
+        return date.date()
+
+    def get_rate(self, instance):
+        data = []
+        teacher_subject = TeacherSubject.objects.filter(user=self._user())
+        teacher_rate = TeacherRate.objects.filter(teacher=self._user())
+        if not teacher_subject:
+            return None
+        else:
+            if not teacher_rate:
+                for subj in teacher_subject:
+                    default_rate = RateTeachers.objects.filter(subject=subj.subject).first().rate
+                    data.append({
+                        'subject': subj.subject.name,
+                        'rate': default_rate
+                    })
+            else:
+                for subj in teacher_subject:
+                    try:
+                        t_rate = TeacherRate.objects.filter(teacher=self._user(), subject=subj.subject).first().rate
+                        data.append({
+                            'subject': subj.subject.name,
+                            'rate': t_rate
+                        })
+                    except Exception:
+                        default_rate = RateTeachers.objects.filter(subject=subj.subject).first().rate
+                        data.append({
+                            'subject': subj.subject.name,
+                            'rate': default_rate
+                        })
+
+        return data
+
+    def get_bill(self, instance):
+        bill = TeacherBankData.objects.filter(user=self._user()).first()
+        serializer = BillSerializer(bill)
+        return serializer.data
+
+
+class HistoryPaymentTeacherSerializer(serializers.ModelSerializer):
+    current_date = serializers.SerializerMethodField()
+    current_time = serializers.SerializerMethodField()
+    status = serializers.SerializerMethodField()
+    subject_name = serializers.SerializerMethodField()
+
+    class Meta:
+        model = HistoryPaymentTeacher
+        fields = ('current_date', 'current_time', 'subject_name', 'status')
+
+    def _user(self):
+        request = self.context.get('request', None)
+        if request:
+            return request.user
+        return None
+
+    def get_status(self, instance):
+        if instance.is_enrollment:
+            return 'Зачисление'
+        elif instance.referral:
+            return 'Зачисление реферальной программы'
+        else:
+            return 'Вывод средств'
+
+    def get_current_date(self, instance):
+        user = self._user()
+        date = current_date(user, instance.payment_date)
+        return date.date()
+
+    def get_current_time(self, instance):
+        user = self._user()
+        date = current_date(user, instance.payment_date)
+        return date.time()
+
+    def get_subject_name(self, instance):
+        if instance.lesson.subject:
+            return instance.lesson.subject.name
         else:
             return None
--- terenoi/finance/views.py
+++ terenoi/finance/views.py
@@ -1,24 +1,34 @@
 from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
-from rest_framework import generics
+from rest_framework import generics, status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from authapp.models import User
-from finance.models import StudentBalance, HistoryPaymentStudent
-from finance.serializers import StudentBalanceSerializer, HistoryPaymentStudentSerializer
+from finance.models import StudentBalance, HistoryPaymentStudent, TeacherBankData, TeacherBalance, HistoryPaymentTeacher
+from finance.serializers import StudentBalanceSerializer, HistoryPaymentStudentSerializer, BillSerializer, \
+    TeacherBalanceSerializer, HistoryPaymentTeacherSerializer
 
 
 class BalanceListView(generics.ListAPIView):
     permission_classes = [IsAuthenticated]
-    serializer_class = StudentBalanceSerializer
 
     def get_queryset(self):
         user = self.request.user
-        queryset = StudentBalance.objects.filter(user=user)
+        if user.is_student:
+            queryset = StudentBalance.objects.filter(user=user)
+        else:
+            queryset = TeacherBalance.objects.filter(user=user)
         return queryset
+
+    def get_serializer_class(self):
+        user = self.request.user
+        if user.is_student:
+            return StudentBalanceSerializer
+        else:
+            return TeacherBalanceSerializer
 
 
 class StudentHistoryPayment(generics.ListAPIView):
@@ -47,3 +57,49 @@ class StudentHistoryPayment(generics.ListAPIView):
             return queryset
         queryset = HistoryPaymentStudent.objects.filter(student=user).order_by('-payment_date')
         return queryset
+
+
+class TeacherHistoryPayment(generics.ListAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = HistoryPaymentTeacherSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        if self.request.query_params.get('subject') and self.request.query_params.get('from'):
+            queryset = HistoryPaymentTeacher.objects.filter(teacher=user,
+                                                            lesson__subject__name=self.request.query_params.get(
+                                                                'subject'),
+                                                            payment_date__range=[self.request.query_params.get('from'),
+                                                                                 self.request.query_params.get(
+                                                                                     'to')]).order_by('-payment_date')
+            return queryset
+        elif self.request.query_params.get('subject'):
+            queryset = HistoryPaymentTeacher.objects.filter(teacher=user,
+                                                            lesson__subject__name=self.request.query_params.get(
+                                                                'subject')).order_by('-payment_date')
+            return queryset
+        elif self.request.query_params.get('from'):
+            queryset = HistoryPaymentTeacher.objects.filter(teacher=user,
+                                                            payment_date__range=[
+                                                                self.request.query_params.get('from'),
+                                                                self.request.query_params.get(
+                                                                    'to')]).order_by('-payment_date')
+            return queryset
+        queryset = HistoryPaymentTeacher.objects.filter(teacher=user).order_by('-payment_date')
+        return queryset
+
+
+class BillCreateView(generics.CreateAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = TeacherBankData.objects.all()
+    serializer_class = BillSerializer
+
+    def perform_create(self, serializer):
+        return serializer.save(user=self.request.user)
+
+
+#
+class BillUpdateView(generics.UpdateAPIView):
+    queryset = TeacherBankData.objects.all()
+    permission_classes = [IsAuthenticated]
+    serializer_class = BillSerializer
