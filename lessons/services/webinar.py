15b7933b1bb6cec84a309e53b8222a1b8771dbbe
olegmaslov <oleg.gnome@yandex.ru>
2022-11-21 23:39:02 +0300

    fixes old bug for group users lesson [bug-fix]

--- lessons/services/webinar.py
+++ lessons/services/webinar.py
@@ -1,11 +1,18 @@
+from time import sleep
+
 from authapp.models import Webinar, WebinarRecord
 from lessons.services.pruffme import PruffMe
 
 
 def create_new_webinar(webinar_pk):
     # lesson = Lesson.objects.get(pk=lesson_pk)
-    print(webinar_pk)
-    webinar = Webinar.objects.get(pk=webinar_pk)
+    webinar = None
+    counter = 0
+    while webinar is None and counter < 100:
+        sleep(1)
+        webinar = Webinar.objects.filter(pk=webinar_pk).first()
+        counter += 1
+
     pruffme = PruffMe()
 
     webinar_response = pruffme.create_webinar(webinar)
