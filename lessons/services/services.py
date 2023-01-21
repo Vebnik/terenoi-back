78631a5a6bd05bff1abd17d44626c86103190747
olegmaslov <oleg.gnome@yandex.ru>
2023-01-21 18:55:08 +0300

    change of datetime format

--- lessons/services/services.py
+++ lessons/services/services.py
@@ -2,10 +2,11 @@ from datetime import datetime
 from authapp.models import User, Group
 from lessons.models import Lesson
 from lessons.services import current_date
+from django.conf import settings
 
 
 def is_free_date(request_date, groups):
-    date = datetime.strptime(request_date, '%Y-%m-%dT%H:%M:%S%z')
+    date = datetime.strptime(request_date, settings.REST_FRAMEWORK.get('DATETIME_FORMAT'))
     student_list = [User.objects.get(pk=item.get('pk')) for item in groups]
     for student in student_list:
         groups = Group.objects.filter(students=student).all()
