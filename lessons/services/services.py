f311aa6fcb10a729dc9bf2094831f34d24ac0329
julia <sorulaijuli@gmail.ru>
2023-01-23 17:14:33 +0300

    rename var

--- lessons/services/services.py
+++ lessons/services/services.py
@@ -14,8 +14,8 @@ def is_free_date(request_date, groups):
             lesson_dates = Lesson.objects.filter(group=group).all().values('date')
             for lesson_date in lesson_dates:
                 student_lesson_date = current_date(student, lesson_date.get('date'))
-                hour_1_lesson = student_lesson_date + datetime.timedelta(hours=1)
+                end_student_lesson_date = student_lesson_date + datetime.timedelta(hours=1)
                 fast_lesson_date = current_date(student, date)
-                if student_lesson_date <= fast_lesson_date < hour_1_lesson:
+                if student_lesson_date <= fast_lesson_date < end_student_lesson_date:
                     return False
     return True
