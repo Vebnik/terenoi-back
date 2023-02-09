872fd28b6bb93213f1db568f0dc9d70c5cfd1d21
julia <sorulaijuli@gmail.ru>
2023-02-09 17:56:20 +0300

    add wishlist model

--- courses/serializers.py
+++ courses/serializers.py
@@ -1,9 +1,10 @@
 from rest_framework import serializers
-from courses.models import Courses, LessonCourse
+from courses.models import Courses, LessonCourse, CourseWishList
 
 
 class CourseSerializer(serializers.ModelSerializer):
     img = serializers.SerializerMethodField()
+    time_duration = serializers.SerializerMethodField()
 
     class Meta:
         model = Courses
@@ -13,9 +14,14 @@ class CourseSerializer(serializers.ModelSerializer):
     def get_img(self, instance):
         return instance.get_course_img()
 
+    def get_time_duration(self, instance):
+        return instance.get_minutes()
+
 
 class CourseRetrieveSerializer(serializers.ModelSerializer):
     img = serializers.SerializerMethodField()
+    time_duration = serializers.SerializerMethodField()
+    author = serializers.SerializerMethodField()
 
     # TODO create permissions for materials
 
@@ -26,9 +32,17 @@ class CourseRetrieveSerializer(serializers.ModelSerializer):
     def get_img(self, instance):
         return instance.get_course_img()
 
+    def get_time_duration(self, instance):
+        return instance.get_minutes()
+
+    def get_author(self, instance):
+        name = f'{instance.author.first_name} {instance.author.last_name}'
+        return name
+
 
 class LessonsCourseAllSerializer(serializers.ModelSerializer):
     img = serializers.SerializerMethodField()
+    time_duration = serializers.SerializerMethodField()
 
     class Meta:
         model = LessonCourse
@@ -37,19 +51,43 @@ class LessonsCourseAllSerializer(serializers.ModelSerializer):
     def get_img(self, instance):
         return instance.get_lesson_img()
 
+    def get_time_duration(self, instance):
+        return instance.get_minutes()
+
 
 class LessonsCourseRetrieveSerializer(serializers.ModelSerializer):
     img = serializers.SerializerMethodField()
     video = serializers.SerializerMethodField()
+    time_duration = serializers.SerializerMethodField()
+    author = serializers.SerializerMethodField()
 
     # TODO create permissions for materials
 
     class Meta:
-        model = Courses
+        model = LessonCourse
         fields = '__all__'
 
     def get_img(self, instance):
         return instance.get_lesson_img()
 
-    def get_video(self,instance):
+    def get_video(self, instance):
         return instance.get_video()
+
+    def get_time_duration(self, instance):
+        return instance.get_minutes()
+
+    def get_author(self, instance):
+        name = f'{instance.author.first_name} {instance.author.last_name}'
+        return name
+
+
+class WishListSerializer(serializers.ModelSerializer):
+    count = serializers.SerializerMethodField()
+
+    class Meta:
+        model = CourseWishList
+        fields = ('count',)
+
+    def get_count(self, instance):
+        wish_count = CourseWishList.objects.filter(course__pk=int(self.context.get('pk'))).count()
+        return wish_count
--- courses/views.py
+++ courses/views.py
@@ -1,27 +1,29 @@
-from rest_framework import generics
+from rest_framework import generics, status
 from rest_framework.permissions import IsAuthenticated
-
-from courses.models import Courses, LessonCourse
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from authapp.models import User
+from courses.models import Courses, LessonCourse, CourseWishList
 from courses.serializers import CourseSerializer, CourseRetrieveSerializer, LessonsCourseAllSerializer, \
-    LessonsCourseRetrieveSerializer
+    LessonsCourseRetrieveSerializer, WishListSerializer
 
 
 class AllCoursesListView(generics.ListAPIView):
-    """Список всех уроков пользователя"""
+    """Список всех курсов"""
     permission_classes = [IsAuthenticated]
     serializer_class = CourseSerializer
     queryset = Courses.objects.all()
 
 
 class CoursesRetrieveView(generics.RetrieveAPIView):
-    """Просмотр одного урока пользователя"""
+    """Просмотр одного курса"""
     permission_classes = [IsAuthenticated]
     serializer_class = CourseRetrieveSerializer
     queryset = Courses.objects.all()
 
 
 class LessonsCourseAllView(generics.ListAPIView):
-    """Просмотр одного урока пользователя"""
+    """Отображение всех уроков курса"""
     permission_classes = [IsAuthenticated]
     serializer_class = LessonsCourseAllSerializer
 
@@ -31,7 +33,58 @@ class LessonsCourseAllView(generics.ListAPIView):
 
 
 class LessonCourseRetrieveView(generics.RetrieveAPIView):
-    """Просмотр одного урока пользователя"""
+    """Просмотр одного урока курса"""
     permission_classes = [IsAuthenticated]
     serializer_class = LessonsCourseRetrieveSerializer
     queryset = LessonCourse.objects.all()
+
+
+class AddCourseWishListView(APIView):
+    """Добавление курса в вишлист"""
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        return User.objects.get(username=self.request.user)
+
+    def post(self, request):
+        user = self.get_object()
+        course = Courses.objects.filter(pk=self.request.data.get('pk')).first()
+        if course:
+            CourseWishList.objects.create(user=user, course=course)
+            data = {'message': True}
+            return Response(data=data, status=status.HTTP_201_CREATED)
+        else:
+            data = {'message': False}
+            return Response(data=data, status=status.HTTP_404_NOT_FOUND)
+
+
+class DeleteCourseWishListView(APIView):
+    """Удаление курса из вишлиста"""
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        return User.objects.get(username=self.request.user)
+
+    def delete(self, request):
+        user = self.get_object()
+        course = Courses.objects.filter(pk=self.request.data.get('pk')).first()
+        wish = CourseWishList.objects.filter(user=user, course=course)
+        if course and wish:
+            wish.delete()
+            data = {'message': True}
+            return Response(data=data, status=status.HTTP_201_CREATED)
+        else:
+            data = {'message': False}
+            return Response(data=data, status=status.HTTP_404_NOT_FOUND)
+
+
+class WishListView(generics.RetrieveAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = WishListSerializer
+    queryset = CourseWishList.objects.all()
+
+    def get_serializer(self, *args, **kwargs):
+        return WishListSerializer(self.queryset, context={'pk': self.kwargs.get('pk')})
+
+
+
